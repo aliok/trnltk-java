3e76e0c7a21f6bfce13067f6af7c6cb59d1d0479
Ali Ok <aliok@aliok.com.tr>
2013-01-01 15:32:53 +0100

    Added a test that validates the simple parse set

--- core/src/test/java/org/trnltk/morphology/contextless/parser/ContextlessMorphologicParserSimpleParseSetCharacterTest.java
+++ core/src/test/java/org/trnltk/morphology/contextless/parser/ContextlessMorphologicParserSimpleParseSetCharacterTest.java
@@ -17,10 +17,7 @@
 package org.trnltk.morphology.contextless.parser;
 
 import com.google.common.base.Joiner;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.*;
 import com.google.common.io.CharStreams;
 import com.google.common.io.InputSupplier;
 import com.google.common.io.LineProcessor;
@@ -97,6 +94,103 @@ public class ContextlessMorphologicParserSimpleParseSetCharacterTest extends Bas
 
             .build();
 
+    private static final ImmutableSet<String> EXPECTED_PARSE_RESULTS_TO_SKIP = new ImmutableSet.Builder<String>()
+            .add("1+Num+Card")
+            .add("70+Num+Card")
+            .add("Num+Distrib")
+
+                    // sacmalik!
+            .add("ikibin+Num").add("sekizonikibindokuzyuzdoksansekiz").add("onsekiz").add("onyedi")
+            .add("doksandokuz").add("bindokuzyüzseksendokuz").add("onbirbindokuzyüzdoksansekiz")
+            .add("binyediyüzotuzdört").add("onbir")
+
+            .add("Verb+Reflex").add("Verb+Stay+")
+
+            .add("incecik+")        // Think about it!
+
+                    //
+            .add("_").add("+Prop").add("+Abbr+")
+
+            .add("Postp")
+            .add("biri+Pron")
+
+            .add("üzer+")
+            .add("üzeri")
+            .add("hiçbiri")
+
+            .add("â").add("î").add("û")
+
+                    // passives to be changed in treebank
+            .add("vurul+Verb").add("dikil")
+
+
+                    // add to master dictionary and check tb for usages
+            .add("önceleri").add("böylesine").add("sonraları")
+
+                    // not sure what to do
+            .add("şakalaş+Verb").add("önceden").add("böylesi")
+
+                    //
+            .add("ayırdet+Verb").add("elatma+Noun").add("varet").add("sözet").add("terket").add("yeral").add("sağol").add("terket").add("yolaç")
+
+                    //
+            .add("a+A3pl+Past")    // yaparlardi
+            .add("a+A3pl+Narr")    // yaparlarmis
+            .add("a+A3pl+Cond")    // yapiyorsalar
+            .add("a+Cop+A3pl")     // hazirdirlar <> hazirlardir , similarly for "Ques"s : midirler
+            .add("a+A2pl+Cond")     // yapmadinizsa
+            .add("a+A2sg+Cond")     // yaptinsa
+
+            .add("kadar")
+            .add("Postp")
+
+            .add("a1,\"yıl+Noun+A3sg+Pnon+Nom\")(2,\"Adv+Since\")").add("yıl+Noun+A3pl+Pnon+Nom\")(2,\"Adv+Since\")") // yildir, yillardir
+
+                    // -sel
+            .add("dinsel+Adj").add("(1,\"toplumsal+Adj\")").add("kişisel+Adj").add("tarihsel").add("içgüdüsel")
+            .add("matematiksel").add("mantıksal").add("deneysel").add("gözlemsel").add("kimyasal")
+            .add("ereksel").add("nedensel").add("fiziksel").add("bütünsel").add("duygusal").add("ruhsal")
+            .add("kavramsal").add("nesnel+Adj").add("algısal").add("içsel").add("geleneksel").add("madensel")
+            .add("hukuksal").add("parasal")
+
+            .add("+Related")    // metodolojik, teknolojik, etc
+            .add("+NotState")
+
+            .add("stoku+Noun+")      // Does optional voicing work? gotta create 2 roots like normal voicing case
+
+            .add("yetkili+Noun").add("ilgili+Noun").add("köylü+Noun")
+
+                    // TODO: check languages like Ingilizce, Almanca, Turkce vs...
+            .add("1,\"ingilizce+Adj\"")
+
+                    // TODO: think about taralı, kapali, takili vs
+            .add("yazılı").add("kapalı").add("takılı").add("taralı")
+
+                    // milletvekilleri, zeytinyaglari etc.
+            .add("milletvekil+Noun+A3pl")
+
+                    // sistemlesme, evlesme, mekanlasma
+            .add("+Noun+A3sg+Pnon+Nom\")(2,\"Verb+Become\")")
+
+                    // yapim, cizim, etc.
+            .add("kestirim").add("kazanım").add("kullanım").add("yapım").add("çizim").add("aşım").add("bileşim")
+
+                    // material used as adjective
+            .add("kadife+Adj").add("mermer+Adj").add("ipek+Adj")
+
+                    // çalı malı
+            .add("malı+Noun+")
+
+            .add("dokun+Verb\")(2,\"Verb+Caus\")")
+            .add("donan+Verb")
+            .add("1,\"dokun+Verb+Pos\")(2,\"Adv+WithoutHavingDoneSo2\")")
+            .add("1,\"barış+Verb+Pos\")(2,\"Noun+Inf1+A3sg+Pnon+Loc\")")
+
+            .add("1,\"kullanım+Noun+A3sg+Pnon+Nom")
+
+            .add("1,\"anlat+Verb\")(2,\"Verb+Able+ Neg\")(3,\"Adv+WithoutHavingDoneSo1\")")        // very complicated!
+            .build();
+
     private HashMultimap<String, ? extends Root> originalRootMap;
 
     public ContextlessMorphologicParserSimpleParseSetCharacterTest() {
@@ -153,20 +247,33 @@ public class ContextlessMorphologicParserSimpleParseSetCharacterTest extends Bas
 
         int unparsable = 0;
         int incorrectParses = 0;
-        int skipped = 0;
+        int skippedSurfaces = 0;
+        int skippedExpectedParseResults = 0;
+
+        final TreeMultiset<String> unparsableSurfaces = TreeMultiset.create();
+        final TreeMultiset<String> incorrectParsedSurfaces = TreeMultiset.create();
 
         for (Pair<String, String> line : lines) {
             final String surfaceToParse = line.getLeft();
             if (SURFACES_TO_SKIP.contains(surfaceToParse)) {
                 if (printSurfaces)
-                    System.out.println("Surface '" + surfaceToParse + "' is not skipped");
-                skipped++;
+                    System.out.println("Surface '" + surfaceToParse + "' is a skippedSurface");
+                skippedSurfaces++;
+                continue;
             }
             final String expectedResult = line.getRight();
+            if (isSkippedExpectedParseResult(expectedResult)) {
+                if (printSurfaces)
+                    System.out.println("Surface with expected parse result '" + expectedResult + "' is a skippedExpectedParseResult");
+                skippedExpectedParseResults++;
+                continue;
+            }
+
             final LinkedList<MorphemeContainer> retrievedResults = this.parse(surfaceToParse);
             if (CollectionUtils.isEmpty(retrievedResults)) {
                 if (printSurfaces)
                     System.out.println("Surface '" + surfaceToParse + "' is not parseable");
+                unparsableSurfaces.add(expectedResult);
                 unparsable++;
             } else {
                 final Collection<String> formattedRetrievedResults = Formatter.formatMorphemeContainersWithDerivationGrouping(retrievedResults);
@@ -175,20 +282,48 @@ public class ContextlessMorphologicParserSimpleParseSetCharacterTest extends Bas
                         System.out.println("Surface '" + surfaceToParse + "' is parseable, but expected result '" + expectedResult + "' is not found!");
                         System.out.println("\t" + Joiner.on("\n\t").join(formattedRetrievedResults));
                     }
+                    incorrectParsedSurfaces.add(expectedResult);
                     incorrectParses++;
                 }
             }
         }
 
-        final int correctParses = numberOfSurfaces - unparsable - incorrectParses - skipped;
+        final int correctParses = numberOfSurfaces - unparsable - incorrectParses - skippedSurfaces - skippedExpectedParseResults;
 
         System.out.println("========SUMMARY===========");
-        System.out.println("Surface count   :\t\t" + numberOfSurfaces);
-        System.out.println("Unparsable      :\t\t" + unparsable);
-        System.out.println("Incorrect parses:\t\t" + incorrectParses);
-        System.out.println("Skipped surfaces:\t\t" + skipped);
-        System.out.println("Correct parses  :\t\t" + correctParses);
-        System.out.println("Correct parse % :\t\t" + (correctParses) * 1.0 / numberOfSurfaces * 100);
+        System.out.println("Surface count             :\t\t" + numberOfSurfaces);
+        System.out.println("Unparsable                :\t\t" + unparsable);
+        System.out.println("Incorrect parses          :\t\t" + incorrectParses);
+        System.out.println("Skipped surfaces          :\t\t" + skippedSurfaces);
+        System.out.println("Skipped parse results     :\t\t" + skippedExpectedParseResults);
+        System.out.println("Correct parses            :\t\t" + correctParses);
+        System.out.println("Correct parse %           :\t\t" + (correctParses) * 1.0 / numberOfSurfaces * 100);
+
+        final ImmutableMultiset<String> unparsableSurfacesByFrequencies = Multisets.copyHighestCountFirst(unparsableSurfaces);
+        final ImmutableMultiset<String> incorrectParsedSurfacesByFrequencies = Multisets.copyHighestCountFirst(incorrectParsedSurfaces);
+
+        System.out.println("=====Unparsable surfaces with occurrence count > 1=====");
+        for (Multiset.Entry<String> entry : unparsableSurfacesByFrequencies.entrySet()) {
+            if (entry.getCount() > 1)
+                System.out.println(entry.getElement() + "\t\t\t" + entry.getCount());
+        }
+
+        System.out.println("=====Incorrect parsed surfaces with occurrence count > 1=====");
+        for (Multiset.Entry<String> entry : incorrectParsedSurfacesByFrequencies.entrySet()) {
+            if (entry.getCount() > 1)
+                System.out.println(entry.getElement() + "\t\t\t" + entry.getCount());
+        }
+    }
+
+    private boolean isSkippedExpectedParseResult(String expectedResult) {
+        if (EXPECTED_PARSE_RESULTS_TO_SKIP.contains(expectedResult))
+            return true;
+        for (String s : EXPECTED_PARSE_RESULTS_TO_SKIP) {
+            if (expectedResult.contains(s))
+                return true;
+        }
+
+        return false;
     }
 
 
