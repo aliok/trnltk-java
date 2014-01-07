265ef77cb6e6f2fda89da0b121e2de3685c06b63
Ali Ok <aliok@apache.org>
2014-01-07 02:02:04 +0100

    UniqueWordFinderApp

--- core/src/app/java/org/trnltk/tokenizer/UniqueWordFinderApp.java
+++ core/src/app/java/org/trnltk/tokenizer/UniqueWordFinderApp.java
@@ -18,9 +18,6 @@ package org.trnltk.tokenizer;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.ImmutableMultiset;
-import com.google.common.collect.Multisets;
 import com.google.common.io.Files;
 import org.apache.commons.lang3.Validate;
 import org.apache.commons.lang3.time.StopWatch;
@@ -31,16 +28,24 @@ import org.trnltk.app.AppRunner;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(AppRunner.class)
-public class UniqueWordFileHistogramApp {
+@Deprecated
+/**
+ * Finds unique words in files in a folder.
+ *
+ * Is not very efficient and required very huge memory if files are big.
+ *
+ * Use WordCountFinderApp instead as it is doing multi-pass and then merging.
+ * @deprecated Use WordCountFinderApp
+ */
+public class UniqueWordFinderApp {
 
-    @App("Goes thru tokenized files and builds word histogram")
+    @App("Goes thru tokenized files, finds unique words")
     public void findWordHistogram() throws InterruptedException {
         final StopWatch taskStopWatch = new StopWatch();
         taskStopWatch.start();
@@ -56,21 +61,19 @@ public class UniqueWordFileHistogramApp {
                 continue;
 
             filesToRead.add(file);
-//            if(filesToRead.size()==8)
-//                break;
         }
 
         int NUMBER_OF_THREADS = 8;
         final ThreadPoolExecutor pool = (ThreadPoolExecutor) Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-        HashMultiset[] multisets = new HashMultiset[NUMBER_OF_THREADS];
-        for (int i = 0; i < multisets.length; i++) {
-            multisets[i] = HashMultiset.create(1000000);
+        Map[] countMaps = new Map[NUMBER_OF_THREADS];
+        for (int i = 0; i < countMaps.length; i++) {
+            countMaps[i] = new HashMap(1000000);
         }
 
         for (int i = 0; i < filesToRead.size(); i++) {
             File file = filesToRead.get(i);
             //noinspection unchecked
-            pool.execute(new HistogramCommand(multisets[i % NUMBER_OF_THREADS], file));
+            pool.execute(new HistogramCommand(countMaps[i % NUMBER_OF_THREADS], file));
         }
 
         pool.shutdown();
@@ -79,21 +82,45 @@ public class UniqueWordFileHistogramApp {
             pool.awaitTermination(3000, TimeUnit.MILLISECONDS);
         }
 
-        System.out.println("Merging sets");
-        final HashMultiset<String> mergeSet = HashMultiset.create(multisets[0].elementSet().size() * NUMBER_OF_THREADS);        //approx
-        for (HashMultiset<String> hashMultiset : multisets) {
-            mergeSet.addAll(hashMultiset);
+        System.out.println("Merging countMaps");
+        final HashMap<String, Integer> mergeMap = new HashMap<String, Integer>(countMaps[0].size() * NUMBER_OF_THREADS);        //approx
+        for (Map<String, Integer> countMap : countMaps) {
+            for (Map.Entry<String, Integer> stringIntegerEntry : countMap.entrySet()) {
+                final String surface = stringIntegerEntry.getKey();
+                final Integer newCount = stringIntegerEntry.getValue();
+                final Integer existingCount = mergeMap.get(surface);
+                if (existingCount == null)
+                    mergeMap.put(surface, newCount);
+                else
+                    mergeMap.put(surface, existingCount + newCount);
+            }
         }
 
-        final ImmutableMultiset<String> sortedMergeSet = Multisets.copyHighestCountFirst(mergeSet);
+        System.out.println("Sorting mergeMaps");
+        final Map<String, Integer> sortedMergeMap = new TreeMap<String, Integer>(new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                Integer x = mergeMap.get(a);
+                Integer y = mergeMap.get(b);
+                if (x.equals(y)) {
+                    return a.compareTo(b);
+                }
+                return y.compareTo(x);
+            }
+        });
 
+        sortedMergeMap.putAll(mergeMap);
+
+
+        System.out.println("Writing to file");
+        int numberOfTokens = 0;
         final File outputFile = new File(parentFolder, "wordHistogram.txt");
         BufferedWriter bufferedWriter = null;
         try {
             bufferedWriter = Files.newWriter(outputFile, Charsets.UTF_8);
-            for (String word : sortedMergeSet.elementSet()) {
-                final int count = sortedMergeSet.count(word);
-                bufferedWriter.write(word + " " + count + "\n");
+            for (Map.Entry<String, Integer> entry : sortedMergeMap.entrySet()) {
+                numberOfTokens += entry.getValue();
+                bufferedWriter.write(entry.getKey() + " " + entry.getValue() + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -111,16 +138,16 @@ public class UniqueWordFileHistogramApp {
         taskStopWatch.stop();
 
         System.out.println("Total time :" + taskStopWatch.toString());
-        System.out.println("Nr of tokens : " + sortedMergeSet.size());
-        System.out.println("Nr of unique tokens : " + sortedMergeSet.elementSet().size());
+        System.out.println("Nr of tokens : " + numberOfTokens);
+        System.out.println("Nr of unique tokens : " + sortedMergeMap.size());
     }
 
     private static class HistogramCommand implements Runnable {
-        private final HashMultiset<String> multiset;
+        private final Map<String, Integer> countMap;
         private final File sourceFile;
 
-        private HistogramCommand(HashMultiset<String> multiset, File sourceFile) {
-            this.multiset = multiset;
+        private HistogramCommand(Map<String, Integer> countMap, File sourceFile) {
+            this.countMap = countMap;
             this.sourceFile = sourceFile;
         }
 
@@ -137,7 +164,11 @@ public class UniqueWordFileHistogramApp {
                     final String line = lines.get(i);
                     final Iterable<String> words = splitter.split(line);
                     for (String word : words) {
-                        multiset.add(word);
+                        final Integer count = countMap.get(word);
+                        if (count == null)
+                            countMap.put(word, 1);
+                        else
+                            countMap.put(word, count + 1);
                     }
                 }
             } catch (IOException e) {
